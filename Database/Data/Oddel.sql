6d446a73d35f5d61ce68b14f2b86151f98c9ed80
Mila Kuch <49004618+milakuch@users.noreply.github.com>
2020-11-25 00:56:38 +0100

    Revert "Submission"
    This reverts commit 81724f18e89d07845965c636cb02da80398f58e9.

--- Database/Data/Oddel.sql
+++
@@ -1,6 +0,0 @@
-insert into Oddel (OddelID, Naziv, id) values (1, 'Business Development', 61343);
-insert into Oddel (OddelID, Naziv, id) values (30, 'Human Resources', 85410);
-insert into Oddel (OddelID, Naziv, id) values (57, 'Engineering', 1476);
-insert into Oddel (OddelID, Naziv, id) values (666, 'Services', 6742);
-insert into Oddel (OddelID, Naziv, id) values (80215, 'Legal', 2959);
-insert into Oddel (OddelID, Naziv, id) values (2856, 'Marketing', 71755);
