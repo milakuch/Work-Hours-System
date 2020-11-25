0af7febdad5daeff0a96ce9d191cc8c0b70eb363
Mila Kuch <49004618+milakuch@users.noreply.github.com>
2020-11-25 01:08:25 +0100

    Commit

--- Database/Data/Menadzer.sql
+++
@@ -1,6 +0,0 @@
-insert into menadzer (id) values ('61343');
-insert into menadzer (id) values ('85410');
-insert into menadzer (id) values ('1476');
-insert into menadzer (id) values ('6742');
-insert into menadzer (id) values ('2959');
-insert into menadzer (id) values ('71755');
--- Database/Data/Modificira.sql
+++
@@ -1,9 +0,0 @@
-insert into menadzira (menadzerkorisnikid,rabotnikkorisnikid) values ('61343','0');
-insert into menadzira (menadzerkorisnikid,rabotnikkorisnikid) values ('1476','25');
-insert into menadzira (menadzerkorisnikid,rabotnikkorisnikid) values ('2959','975');
-insert into menadzira (menadzerkorisnikid,rabotnikkorisnikid) values ('71755','54');
-
-insert into menadzira (menadzerkorisnikid,rabotnikkorisnikid) values ('85410','2');
-insert into menadzira (menadzerkorisnikid,rabotnikkorisnikid) values ('6742','3');
-insert into menadzira (menadzerkorisnikid,rabotnikkorisnikid) values ('71755','7');
-insert into menadzira (menadzerkorisnikid,rabotnikkorisnikid) values ('71755','367');
